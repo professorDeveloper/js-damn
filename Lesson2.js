5590ecf3e9b3376f91ea1c095e13f26dbcd4356b
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-02-14 15:08:17 +0500

    Day-3 Operators learn

--- Lesson2.js
+++ Lesson2.js
@@ -2,27 +2,34 @@
  * Copyright (c) 2025.
  */
 
-let num1 = Number(prompt("Enter number1:"))
-let num2 = Number(prompt("Enter number2:"))
+// let num1 = Number(prompt("Enter number1:"))
+// let num2 = Number(prompt("Enter number2:"))
+
 // let bolinma = (num1 / num2) - ((num1 / num2) % 1);
-//
-//
 // console.log(`${num1} / ${num2} = ${bolinma} (bolinma)`)
 // console.log(`${num1} - ${num2} = ${num1 - num2} (ayirma)`)
 // console.log(`${num1} + ${num2} = ${num1 + num2} (yig'indi)`)
 // console.log(`${num1} %  ${num2} = ${num1 % num2} (qoldiq)`)
+//
+// // 1-usul
+// num3 = num1
+// num1 = num2
+// num2 = num3
+// console.log(`Num1: ${num1}`)
+// console.log(`Num2: ${num2}`)
+//
+// // 2-usul
+// num1 = num1 + num2
+// num2 = num1 - num2
+// num1 = num1 - num2
+// console.log(`Num1: ${num1}`)
+// console.log(`Num2: ${num2}`)
+//
 
-// 1-usul
-num3 = num1
-num1 = num2
-num2 = num3
-console.log(`Num1: ${num1}`)
-console.log(`Num2: ${num2}`)
+//Task2
 
-// 2-usul
-num1 = num1 + num2
-num2 = num1 - num2
-num1 = num1 - num2
-console.log(`Num1: ${num1}`)
-console.log(`Num2: ${num2}`)
-
+let userNum = prompt('sonni kiriting:')
+console.log(`Son juftmi ${userNum % 2 === 0}`)
+console.log(`Ikki xonali sonmi  ${userNum > 9 && userNum < 100}`)
+console.log(`Ikki xonali musbat sonmi  ${userNum > 0}`)
+console.log(`2 xonali juft musbat sonmi ${(userNum % 2 === 0) && (userNum > 9 && userNum < 100) && (userNum > 0)}`)
---
+++ Lesson2Homework.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2025.  Azamov
+ */
+
+let num = prompt("Enter number:");
+
+
+let bolinma5 = num % 5 === 0;
+let is3Xonali = num.length === 3;
+let is3XonaliMusbat = num > 99 && num < 1000;
+let is4XonaliManfiyJuft = num % 2 === 0 && num.length === 4;
+
+console.log(`Raqam 5 ga bolinadimi :${bolinma5}`)
+console.log(`Raqam 3 xonalimi :${is3Xonali}`)
+console.log(`Raqam 3 xonali musbatmi  :${is3XonaliMusbat}`)
+console.log(`Raqam 4 xonali manfiymi  :${is4XonaliManfiyJuft}`)
+
+
+// Another Task
+//Task-1
+let a = 10
+console.log(`Birinchi qiymat ${a}`)
+
+a = 20;
+console.log(`2-qiymat :${a}`)
+
+//Task-2
+let b = 1 + 2 + 3;
+console.log("b ning qiymati:", b);
+
+//Task-3
+let a3 = 1.5;
+let b3 = 0.75;
+let result3 = a3 + b3;
+console.log("Natija: " + result3)
+
+// Task4
+let a4 = 13;
+let b4 = 5;
+console.log(`Qoldiq: ${a4 % b4}`)
+
+//Task-5
+let num5 = 2;
+console.log(`${num5} ning 10 darajasi : ${num5 ** 10}`)
+
+
+//Task-6
+const PI = 3.14;
+let radius = 12;
+let area = PI * radius * radius;
+let perimeter = 2 * PI * radius;
+console.log(`${radius} dagi aylananing yuzi ${area}`)
+console.log(`${radius} dagi aylananing perimetri ${perimeter}`)
+
+
+//Task-7
+let a7 = "3+3+1";
+console.log(a7)
