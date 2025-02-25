50e63c37c1c1f1a65ea1dca63e0a3d0032583a1c
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-02-25 16:36:34 +0500

    Day 7 enjoy subject youre

--- HomeworkWhile.js
+++ HomeworkWhile.js
@@ -2,3 +2,25 @@
  * Copyright (c) 2025.  Azamov
  */
 
+let i = 1
+while (i <= 10) {
+    console.log(i)
+    i++
+}
+
+// Task2
+let foydalanuvchiSon = Number(prompt("Num:"))
+
+let i2 = 1
+while (i2 <= foydalanuvchiSon) {
+    console.log(`${i2}x${foydalanuvchiSon}=${i2 * foydalanuvchiSon}`)
+}
+
+//Task3
+let num3 = 20
+let i3 = 1
+while (i3 <= num3) {
+    if (i3 % 2 === 0) {
+        console.log(i3)
+    }
+}
