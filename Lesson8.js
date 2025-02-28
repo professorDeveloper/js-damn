093e92c4a2a33e0c0b391c93d9b553cf64d90f4b
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-02-28 16:37:19 +0500

    so

--- Lesson8.js
+++ Lesson8.js
@@ -25,3 +25,29 @@ for (let i = matn.length - 1; i >= 0; i--) {
     }
 }
 console.log(lastWord)
+
+//Homework 1
+let count = 0;
+for (let i = 1; i <= 10000; i++) {
+    if (i % 4 === 0) {
+        count++;
+        if (count === 25) continue;
+        console.log(i);
+    }
+}
+
+
+//Homework 2
+// Foydalanuvchidan matn kiritishni so'rash
+let userInput = prompt("Matn kiriting:");
+
+// Raqamlarni sanash
+let countNumbers = 0;
+for (let i = 0; i < userInput.length; i++) {
+    let char = userInput[i];
+    if (char >= '0' && char <= '9') {
+        countNumbers++;
+    }
+}
+
+console.log(`Matnda ${countNumbers} ta raqam bor.`);
