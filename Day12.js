fb88456df753468fb72400080e4d23dea5b2859c
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-03-04 16:57:39 +0500

    Day 12

--- Day12.js
+++ Day12.js
@@ -2,3 +2,23 @@
  * Copyright (c) 2025.  Azamov
  */
 
+// let massive =[1,2,3,4,4]
+// Location.call(
+//
+// )
+
+const sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(sonlar);
+// sonlar.unshift(12, 123, 123)
+//
+// console.log(sonlar);
+//
+// sonlar.splice(0,-1,-11)
+// console.log(sonlar)
+//
+// // sonlar.push(12, 123, 123)
+// console.log(sonlar)
+
+sonlar.concat(3)
+
+console.log(sonlar.every((value) => value %2 ===0));
