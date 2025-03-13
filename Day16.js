9c21664b30446cfecc1ac0d0fe71c45c4c0219d5
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-03-13 16:20:32 +0500

    Day 16 Nothing Every Day )

--- Day16.js
+++ Day16.js
@@ -1,17 +1,1 @@
-/*
- * Copyright (c) 2025.  Azamov
- */
-
-
-alert(demoAdd(1,2))
-
-
-/***
- @param {number} son1
- @param {number} son2
- @returns {number}
- ***/
-function demoAdd(son1, son2) {
-    return son1 + son2
-}
-
+const productts = [];
