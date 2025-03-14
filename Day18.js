c566bcd2dfda6ae2ec7c58e1beb3cc2aeb72a0af
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-03-14 16:31:00 +0500

    Js is always trash damn js

--- Day18.js
+++ Day18.js
@@ -2,3 +2,24 @@
  * Copyright (c) 2025.  Azamov
  */
 
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         sum += arr[i];
+//     }
+// }
+// console.log(sum);
+
+//shunday funksiya yarating  funksiya ichida userdan prompt orqali  son olinsin. va shu olingan son callback funksiyaga berilsin
+
+
+customPrompt(num => {
+    console.log(num)
+})
+
+function customPrompt(callBack) {
+    let num = Number(prompt("Num:"))
+    callBack(num)
+}
